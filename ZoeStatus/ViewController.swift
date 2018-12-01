7e172bba1f3d32fcd4a582169a41874ad82e49c6
Dr. Guido Mocken <mocken@freenet.de>
2018-12-01 16:18:40 +0100

    Initial Commit

---
+++ ZoeStatus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZoeStatus
+//
+//  Created by Dr. Guido Mocken on 01.12.18.
+//  Copyright © 2018 Dr. Guido Mocken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
