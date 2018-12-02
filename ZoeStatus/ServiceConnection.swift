96862ec027afb0c311a30bbb2288139f1df93740
Dr. Guido Mocken <mocken@freenet.de>
2018-12-02 19:00:30 +0100

    Proof of concept: Login and subsequent retrieval of battery state basically working.

---
+++ ZoeStatus/ServiceConnection.swift
@@ -0,0 +1,203 @@
+//
+//  ServiceConnection.swift
+//  ZoeStatus
+//
+//  Created by Dr. Guido Mocken on 02.12.18.
+//  Copyright © 2018 Dr. Guido Mocken. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ServiceConnection {
+
+    var activationCode: String?
+    var userName:String?
+    var password:String?
+    var vehicleIdentification:String?
+    var token:String?
+    var refresh_token:String?
+    
+    let baseURL = "https://www.services.renault-ze.com/api"
+    
+    
+    func login () {
+    
+        struct Credentials: Codable {
+            let username: String
+            let password: String
+        }
+        
+        let credentials = Credentials(username: userName!,
+                          password: password!)
+        guard let uploadData = try? JSONEncoder().encode(credentials) else {
+            return
+        }
+        
+//        print(String(data: uploadData, encoding: .utf8)!)
+        print("Sending: "+String(decoding: uploadData, as: UTF8.self))
+
+        let loginURL = baseURL + "/user/login"
+        let url = URL(string: loginURL)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.uploadTask(with: request, from: uploadData) { data, response, error in
+            if let error = error {
+                print ("error: \(error)")
+                return
+            }
+            guard let response = response as? HTTPURLResponse,
+                (200...299).contains(response.statusCode) else {
+                    print ("server error")
+                    return
+            }
+            if let mimeType = response.mimeType,
+                mimeType == "application/json",
+                let data = data,
+                let dataString = String(data: data, encoding: .utf8) {
+                print ("got data: \(dataString)")
+                
+          
+
+                
+                struct loginResult: Codable {
+                    var token: String
+                    var refresh_token: String
+                    var user: User
+                    struct User: Codable {
+                        var id: String
+                        var locale: String
+                        var country: String
+                        var timezone: String
+                        var email: String
+                        var first_name: String
+                        var last_name: String
+                        var phone_number: String
+                        var vehicle_details: Vehicle_Details
+                        struct Vehicle_Details: Codable {
+                            var timezone: String
+                            var VIN: String
+                            var activation_code: String
+                            var phone_number: String
+                        }
+                        var scopes:[String]
+                        var active_account: String
+                        var associated_vehicles:[Associated_Vehicles]
+                        struct Associated_Vehicles: Codable {
+                            var VIN: String
+                            var activation_code: String
+                            var user_id: String
+                        }
+                        var gdc_uid: String
+                    }
+                }
+                
+                do{
+                    let decoder = JSONDecoder()
+                    let result = try decoder.decode(loginResult.self, from: data)
+                    
+                    /*
+                    print("Number of vehicles: " + String(format: "%d", result.user.associated_vehicles.count))
+                    var i=0
+                    for vehicle in result.user.associated_vehicles{
+                        print("\(i):"+vehicle.VIN)
+                        i+=1;
+                    }
+                    */
+                    
+                    self.vehicleIdentification = result.user.vehicle_details.VIN
+                    self.activationCode = result.user.vehicle_details.activation_code
+                    self.refresh_token = result.refresh_token
+                    self.token = result.token
+
+                    
+                } catch {
+                    print (error)
+                }
+                
+                
+                
+                
+                
+            }
+        }
+        task.resume()
+        
+        
+        
+    }
+    
+    
+    
+
+
+    func batteryState (delegate:ViewController) {
+        
+        let batteryURL = baseURL + "/vehicle/" + vehicleIdentification! + "/battery"
+
+        let tString = ""
+        let uploadData = tString.data(using: String.Encoding.utf8)
+        
+        let url = URL(string: batteryURL)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Bearer \(token!)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.uploadTask(with: request, from: uploadData) { data, response, error in
+            if let error = error {
+                print ("error: \(error)")
+                return
+            }
+            guard let response = response as? HTTPURLResponse,
+                (200...299).contains(response.statusCode) else {
+                    print ("server error")
+                    return
+            }
+            if let mimeType = response.mimeType,
+                mimeType == "application/json",
+                let data = data,
+                let dataString = String(data: data, encoding: .utf8) {
+                print ("got data: \(dataString)")
+                
+                
+                
+                struct batteryStatus: Codable{
+                    var charging: Bool
+                    var plugged: Bool
+                    var charge_level: UInt8
+                    var remaining_range: Float
+                    var last_update: UInt64 //TimeInterval
+              //      var charging_point: String
+              //      var remaining_time: Int
+                }
+                /*
+                 The remaining_range is in Kilometres. The last_update is a Unix timestamp. The remaining_time is in minutes. The charging_point is available only when plugged is true. The remaining_time is available only when charging is true.
+                 */
+                
+                do{
+                    let decoder = JSONDecoder()
+                    let product = try decoder.decode(batteryStatus.self, from: data)
+                    print(product.charge_level)
+                    
+                    DispatchQueue.main.async {
+                        delegate.batteryState.text = String(format: "%3d%%", product.charge_level)
+
+                    }
+                    
+                    
+                } catch {
+                    print (error)
+                }
+                
+                
+                
+                
+                
+            }
+        }
+        task.resume()
+    }
+    
+}
--- ZoeStatus/ViewController.swift
+++ ZoeStatus/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var percent:UInt8 = 0
+    
+    let baseURL =  "https://www.services.renault-ze.com/api"
 
+    
+    var sc=ServiceConnection()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UserDefaults.standard.register(defaults: [String : Any]())
+
+        
+        let userDefaults = UserDefaults.standard
+        let userName = userDefaults.string(forKey: "userName_preference")
+        if userName != nil {
+            print("User name = \(userName!)")
+        }
+        
+        if false { // PERSONAL VALUES MUST BE REMOVED BEFORE GOING PUBLIC! ALSO DO NOT COMMIT TO GIT!
+            let defaultUserName = "your@email.address"
+            let defaultPassword = "secret password"
+
+            userDefaults.setValue(defaultUserName, forKey: "userName_preference")
+            userDefaults.setValue(defaultPassword, forKey: "password_preference")
+            
+            userDefaults.synchronize()
+        }
+        
+        // load settings
+        
+        sc.userName = userDefaults.string(forKey: "userName_preference")
+        sc.password = userDefaults.string(forKey: "password_preference")
+
+        sc.login()
+
+
     }
 
+    @IBOutlet var batteryState: UILabel!
+    
+    
 
+    @IBAction func refreshButtonPressed(_ sender: Any) {
+        
+        sc.batteryState(delegate:self)
+        
+       
+    }
+ 
 }
 
